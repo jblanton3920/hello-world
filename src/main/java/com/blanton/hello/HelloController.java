e4f232d6ce0047f6e6865ab0fa19bdfaf028141c
jacob <jacobblanton28@gmail.com>
2022-01-22 13:16:47 -0500

    Added log4j2 spring implementation with the hello world service

--- src/main/java/com/blanton/hello/HelloController.java
+++ src/main/java/com/blanton/hello/HelloController.java
@@ -1,5 +1,7 @@
 package com.blanton.hello;
 
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,17 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("hellos")
+@Slf4j
+@NoArgsConstructor
 public class HelloController {
-
-    public HelloController() { }
 
     @GetMapping(value = {"/",""})
     public String helloDefault() {
+        log.trace("Client call to the default hello path, who knows what they want?");
         return "You have reached the hello world app, where everyone says hello!";
     }
 
     @GetMapping(value = "/{name}")
     public String helloByName(@PathVariable("name") String name) {
+        log.info("Telling {} hello!", name);
         return "Hello " + name + "!";
     }
 }
